9f8783650f4744ef8d50d4eef47366f76f12832b
= <=>
2022-09-28 00:08:16 -0500

    Configurar alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,69 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@components": path.resolve(__dirname, "src/containers/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@icons": path.resolve(__dirname, "src/assets/icons/"),
+      "@logos": path.resolve(__dirname, "src/assets/icons/"),
     },
-    mode:'development',
-    resolve:{
-        extensions:['.js','.jsx'],
-    },
-    module : {
-        rules:[
-            {
-                test:/\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                use:[
-                    {
-                        loader: 'html-loader'
-                    }
-                ]
-            },
-            {
-                test:/\.s[ac]ss$/i,
-                use: [
-                  "style-loader",
-                  "css-loader",
-                  "sass-loader",
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
-    ],
-    devServer: {
-        static: {
-          directory: path.join(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
           },
-        compress: true,
-        port: 3005,
-    }
-}
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3005,
+  },
+};
